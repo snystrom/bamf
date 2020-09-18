7cd34a6cf31dcf761d3354d3df8e92e225b325f9
snystrom <snystrom@users.noreply.github.com>
2020-09-18 06:55:46 -0400

    Modify to take input path, better handling of -b.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use structopt::StructOpt;
 // https://rust-cli.github.io/book/tutorial/index.html
 #[derive(StructOpt)]
 struct Cli {
+    #[structopt(parse(from_os_str))]
+    infile: std::path::PathBuf,
     #[structopt(default_value = "0", short = "a", long = "above")]
     above: i64,
-    #[structopt(default_value = "1000", short = "b", long = "below")]
-    below: i64,
+    // TODO:
+    // args.below needs a NONE default value, not 1000
+    #[structopt(short = "b", long = "below")]
+    below: Option<i64>,
     #[structopt(short, long)]
-    size: bool
+    summary: bool
+    //#[structopt(short, long)]
+    //input: bam::Reader::from_path()
     //path: bam::Reader::from_path()
 
 }
@@ -17,14 +23,16 @@ struct Cli {
 
 
 fn main() {
-    let bam_path = &"../../test_data/yw-3LW-aH3K27me3-experiment-20-Wing-30-Std-rep1-Sup_dm6_trim_q5_dupsRemoved.bam";
+
+    let args = Cli::from_args();
+
+    //let bam_path = &"../../test_data/yw-3LW-aH3K27me3-experiment-20-Wing-30-Std-rep1-Sup_dm6_trim_q5_dupsRemoved.bam";
+    let bam_path = &args.infile;
     let mut bam = bam::Reader::from_path(bam_path).unwrap();
     let header = bam::Header::from_template(bam.header());
     let mut out = bam::Writer::from_path(&"examples/out.bam", &header, bam::Format::BAM).unwrap();
 
-    let args = Cli::from_args();
-
-    if args.size == true {
+    if args.summary == true {
         let mut init = true;
         let mut min_val: i64 = 0;
         let mut max_val: i64 = 0;
@@ -56,14 +64,34 @@ fn main() {
         let record = r.unwrap();
         // negative insert sizes come from reverse-strand alignment
         // so, take absolute value of size for filtering
-        if record.insert_size().abs() <= args.below {
-            out.write(&record).unwrap();
-           
-        }
+        let insert_size = record.insert_size().abs();
 
-        if record.insert_size().abs() >= args.above {
-            out.write(&record).unwrap();
+        // TODO:
+        // args.below needs a NONE default value,
+        // but if set, filter reads in a range above:below
+        // How to implement this check?
+        // Current implementation is correct, but:
+        // LEARN:
+        // Is there better idiomatic Rust approach?
 
+        // TODO: TESTING:
+        // Will short circuiting work? like so:
+        // insert_size >= args.above || args.below.is_some() && insert_size <= args.below.unwrap()
+
+        // Doesn't work:
+        // short circuits on first arg since false (duh).
+        //if insert_size >= args.above || args.below.is_some() && insert_size <= args.below.unwrap() {
+        //    out.write(&record).unwrap();
+        //}
+
+        if args.below.is_some() {
+            if insert_size <= args.below.unwrap() && insert_size >= args.above {
+                out.write(&record).unwrap();
+            }
+        } else {
+            if insert_size >= args.above {
+                out.write(&record).unwrap();
+            }
         }
 
     }
